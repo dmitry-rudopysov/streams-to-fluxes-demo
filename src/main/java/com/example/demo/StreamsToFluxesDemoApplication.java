036e28f880f9470f848529cb3ce0b9599f8ebb39
Dmitry Rudopysov <rudopysov.ds@gmail.com>
2022-05-27 10:48:32 +0300

    use ascii helper;

--- src/main/java/com/example/demo/StreamsToFluxesDemoApplication.java
+++ src/main/java/com/example/demo/StreamsToFluxesDemoApplication.java
@@ -14,6 +14,8 @@ import java.util.Random;
 import java.util.stream.IntStream;
 
 import static com.example.demo.model.Currency.getRandomCurrency;
+import static com.example.demo.util.AsciiHelper.GREEN;
+import static com.example.demo.util.AsciiHelper.RESET;
 
 @Slf4j
 @SpringBootApplication
@@ -36,7 +38,7 @@ public class StreamsToFluxesDemoApplication implements CommandLineRunner {
                 .map(__ -> random.nextInt(100, 1000))
                 .mapToObj(BigDecimal::new)
                 .map(rub -> currencyCalculator.getRates(getRandomCurrency(), rub))
-                .peek(result -> log.info("\u001b[32m Conversion result {} \u001b[0m", result))
+                .peek(result -> log.info(GREEN.getCode() + "Conversion result {}" + RESET.getCode(), result))
                 .toList();
 
 
