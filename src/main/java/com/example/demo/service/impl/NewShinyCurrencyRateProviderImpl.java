428ec986a1519fbaa2f263d8e5880bb20a3d3ff9
Dmitry Rudopysov <rudopysov.ds@gmail.com>
2022-05-28 14:24:04 +0300

    use Mono.delay() instead of thread blocking sleep;

--- src/main/java/com/example/demo/service/impl/NewShinyCurrencyRateProviderImpl.java
+++ src/main/java/com/example/demo/service/impl/NewShinyCurrencyRateProviderImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
+import java.time.Duration;
 import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
@@ -24,18 +25,12 @@ public class NewShinyCurrencyRateProviderImpl implements NewShinyCurrencyRatePro
     @Override
     public Mono<BigDecimal> getRateMono(Currency currency) {
         return Mono.fromFuture(CompletableFuture.supplyAsync(() -> {
-            var i = counter.incrementAndGet();
-            log.info("{} Request for currency {}", i, currency);
-            sleep();
-            var fractionalPart = new Random().nextInt(0, 100);
-            var integralPart = currencyMap.get(currency);
-            return new BigDecimal(integralPart * 100 + fractionalPart).movePointLeft(2);
-        }));
-    }
-
-    private void sleep() {
-        try {
-            Thread.sleep(1000);
-        } catch (Exception ignore) {/*NoP*/}
+                    var i = counter.incrementAndGet();
+                    log.info("{} Request for currency {}", i, currency);
+                    var fractionalPart = new Random().nextInt(0, 100);
+                    var integralPart = currencyMap.get(currency);
+                    return new BigDecimal(integralPart * 100 + fractionalPart).movePointLeft(2);
+                }))
+                .delayElement(Duration.ofSeconds(1));
     }
 }
